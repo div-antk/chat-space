dc22560a4e98ed1132e62ee799e1491fb28d1701
div-antk <51283468+owaii@users.noreply.github.com>
2019-07-06 09:55:53 +0900

    メッセージ非同期通信の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,16 +4,19 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+      respond_to do |format|
+        format.html
+        format.json
+      end
   end
 
   def create
     @message = @group.messages.new(message_params)
-    respond_to do |format|
-      if @message.save
+    if @message.save
+      respond_to do |format|
         format.html {redirect_to group_messages_path(@group)}
-        format.json {render json: @message}
-    end
-    
+        format.json
+      end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを書いてください。。'
@@ -29,5 +32,7 @@ class MessagesController < ApplicationController
 
   def set_group
     @group = Group.find(params[:group_id])
+    @users = @group.users
+    Time.zone ='Tokyo'
   end
 end
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,6 +1,5 @@
+json.(@message, :content, :image)
+json.created_at @message.created_at.strftime('%Y/%m/%d %R')
+json.user_name @message.user.name
+# json.image.url @message.image.url
 json.id @message.id
-json.content @message.content
-json.image @message.image
-json.group_id @message.group_id
-json.user_id @messsage.user_id
-json.time @message.created_at
